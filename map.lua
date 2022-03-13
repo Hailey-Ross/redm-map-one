59037957ed70a8e8cf5f7528e44bdf6461d2611c
Hailey <26784904+Hailey-Ross@users.noreply.github.com>
2022-03-12 22:44:49 -0700

    Create map.lua

---
+++ map.lua
@@ -0,0 +1,4 @@
+--spawnpoint 'player_three' { x = -262.849, y = 793.404, z = 118.087 }
+--spawnpoint 'player_zero' { x = -262.849, y = 793.404, z = 118.087 }
+spawnpoint 'player_three' { x = -358.770, y = -1132.439, z = 42.055 } --Initial Spawn point now moved to out in the middle of a lake on a island
+spawnpoint 'player_zero' { x = -358.770, y = -1132.439, z = 42.055 }
